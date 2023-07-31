7c354c01bf211e768994d336608356548f573284
hikaru <827394320@qq.com>
2023-07-31 16:56:52 +0800

    🐞 fix: 修复控制台输出以及日志记录换行格式混乱的问题

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ program
 			let reg = /[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?/g
 			ip = ip.match(reg)
 			if (ip == null) {
-				formatOutput("参数错误", "请填入合法的IP地址/域名", "error")
+				formatOutput("error", "请填入合法的IP地址/域名", "参数错误")
 			} else {
 				let params = [`${ip}`]
 				if (options.count) {
@@ -51,11 +51,38 @@ program
 
 				ping.stdout.on("data", (data: any) => {
 					let str = iconv.decode(data, "gbk").trimStart()
+					const reg =
+						/(?:来自\s(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|[\w.-]+)\s的回复:\s字节=\d+\s时间[<=>]\d+ms\sTTL=\d+)|(?:来自\s(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|[\w.-]+)\s的回复:\s无法访问目标主机。)|(?:PING：传输失败。常见故障。)|(?:请求超时。)/g
+					let ping_str = str.match(reg) === null ? "" : str.match(reg)![0]
+					let ping_end = str.split(reg).filter((item) => item !== "" && item !== "\r\n")[0]
+						? str
+								.split(reg)
+								.filter((item) => item !== "" && item !== "\r\n")[0]
+								.trimStart()
+						: str.split(reg).filter((item) => item !== "" && item !== "\r\n")[0]
+
 					if (!options.nolog) {
-						fs.appendFileSync(path.join(os.homedir(), "/Desktop/nping_log.txt"), formatOutput(getCurrentTime(), str, "success"))
+						if (ping_str !== "") {
+							fs.appendFileSync(path.join(os.homedir(), "/Desktop/nping_log.txt"), formatOutput("success", ping_str, getCurrentTime()))
+						}
+						if (ping_end) {
+							fs.appendFileSync(path.join(os.homedir(), "/Desktop/nping_log.txt"), formatOutput("success", ping_end))
+						}
 					} else {
-						formatOutput(getCurrentTime(), str, "success")
+						if (ping_str) {
+							formatOutput("success", ping_str, getCurrentTime())
+						}
+						if (ping_end) {
+							formatOutput("success", ping_end)
+						}
 					}
+				})
+
+				ping.stdout.on("error", (chunk: any) => {
+					console.log(chunk)
+				})
+				ping.stdout.on("end", () => {
+					fs.appendFileSync(path.join(os.homedir(), "/Desktop/nping_log.txt"), "\r\n\r\n\r\n")
 				})
 			}
 		}
--- src/utils/formatOutput.ts
+++ src/utils/formatOutput.ts
@@ -1,16 +1,31 @@
 import chalk from "chalk"
 
-export const formatOutput = (title: string, out: string, type: "success" | "error" | "warning") => {
-	switch (type) {
-		case "success":
-			console.log(chalk.bgGreen.white(` ${title} `), chalk.green(` ${out} `))
-			break
-		case "error":
-			console.log(chalk.bgRed.white(` ${title} `), chalk.red(` ${out} `))
-			break
-		case "warning":
-			console.log(chalk.bgYellow.white(` ${title} `), chalk.yellow(` ${out} `))
-			break
+export const formatOutput = (type: "success" | "error" | "warning", out: string, title?: string) => {
+	if (!title) {
+		switch (type) {
+			case "success":
+				console.log(chalk.green(` ${out.trimEnd()} `))
+				break
+			case "error":
+				console.log(chalk.red(` ${out.trimEnd()} `))
+				break
+			case "warning":
+				console.log(chalk.yellow(` ${out.trimEnd()} `))
+				break
+		}
+		return `${out}`
+	} else {
+		switch (type) {
+			case "success":
+				console.log(chalk.bgGreen.white(` ${title} `), chalk.green(` ${out} `), "\r\n")
+				break
+			case "error":
+				console.log(chalk.bgRed.white(` ${title} `), chalk.red(` ${out} `), "\r\n")
+				break
+			case "warning":
+				console.log(chalk.bgYellow.white(` ${title} `), chalk.yellow(` ${out} `), "\r\n")
+				break
+		}
+		return `${title} ${out}\r\n`
 	}
-	return `${title} ${out}`
 }
